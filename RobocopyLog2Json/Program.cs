b5b9326b998f6c4aba178225e752a4371cdaee1a
Mickaël FRANCOIS <mickael.francois@fpc-france.com>
2018-10-24 12:48:18 +0200

    Ajoutez des fichiers projet.

---
+++ RobocopyLog2Json/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobocopyLog2Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
